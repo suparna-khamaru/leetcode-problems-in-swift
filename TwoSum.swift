ccc661f6738c7198ee189a4876e8cb74f32646d8
Suparna Khamaru <suparna3107@gmail.com>
2020-08-21 00:34:11 +0530

    1 pass hash table

--- TwoSum.swift
+++ TwoSum.swift
@@ -20,3 +20,23 @@ print(twoSum([3,2,3], 6))     // 0,2
 
 //Approach 2: One-pass hash table
 
+func twoSum2(_ nums: [Int], _ target: Int) -> [Int] {
+        
+        var hashTable = [Int:Int]()
+        
+        for (key, value) in nums.enumerated() {
+            let subtracted = target - value
+            if let match = hashTable[subtracted] {
+                return [match, key]
+            }
+            else {
+                hashTable[value] = key
+            }
+        }
+        return [-1, -1]
+    }
+}
+
+print(twoSum2([2,7,11,15], 9))  // 0,1
+print(twoSum2([3,2,4], 6))      // 1,2
+print(twoSum2([3,2,3], 6))      // 0,2
