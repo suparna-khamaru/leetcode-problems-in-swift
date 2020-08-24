32935dd44c39e8170ca80dfbc274d3090778b2e3
Suparna Khamaru <suparna3107@gmail.com>
2020-08-24 23:31:47 +0530

    sliding window

--- 3.LongestSubstringWithoutRepeatingCharacters.swift
+++ 3.LongestSubstringWithoutRepeatingCharacters.swift
@@ -1,22 +1,23 @@
  func lengthOfLongestSubstring(_ s: String) -> Int {
         
-        var newArray = [Character: Int]()
-        let characters = Array(s)
-        var left=0, right = 0
-        var maxLength = 0
-        
-        while right < characters.count {
-            let char = characters[right]
-            if (newArray[char] != nil) {
-                left = max(newArray[char]! + 1, left)
-            }
-            newArray[char] = right
-            maxLength = max(maxLength, right-left+1)
-            right+=1
+    var maxLength = 0, left = 0, right = 0
+    let char = Array(s)
+    var hashmap = [Character: Int]()
+
+    while (right < char.count && left < char.count) {
+     
+        let character = char[right]
+        if (hashmap[character] != nil) {
+            left = max(hashmap[character]!+1 , left)
         }
-       return maxLength
+        hashmap[character] = right
+        maxLength = max(maxLength, right-left+1)
+        right+=1
+    }
+    return maxLength
 }
 
 print(lengthOfLongestSubstring("abcabcbb"))   //3     -> abc
 print(lengthOfLongestSubstring("bbbbb"))      //1     -> b
 print(lengthOfLongestSubstring("pwwkew"))     //3     -> wke
+print(demo(s: "abcdefabcddabcdefghi"))        //9     -> abcdefghi
